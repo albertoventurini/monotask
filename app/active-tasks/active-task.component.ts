dae50b9239037cf02b1d7627f887b2f9ae09788c
Alberto Venturini <aventurini@gmail.com>
2016-12-28 21:09:56 +0200

    Added timer for tasks

--- app/active-tasks/active-task.component.ts
+++ app/active-tasks/active-task.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import { Subscription } from "rxjs/Subscription";
 
 import { Task } from '../task/task';
 
@@ -6,13 +8,32 @@ import { Task } from '../task/task';
     selector: 'active-task',
     template: `
         <div>
-            {{task.name}} - {{task.timeInSeconds}}
-            <button>Start</button>
-            <button>Stop</button>
-            <button>Remove</button>
+            {{task.name}} - {{seconds}}
+            <button (click)="onStart()">Start</button>
+            <button (click)="onStop()">Stop</button>
+            <button (click)="onRemove()">Remove</button>
         </div>
     `
 })
 export class ActiveTaskComponent {
     @Input() task: Task;
+    private seconds: number;
+
+    private subscription: Subscription;
+
+    private observableTimedSequence = Observable.interval(1000);
+
+    ngOnInit() {
+        this.seconds = this.task.timeInSeconds;
+    }
+
+    onStart() {
+        this.subscription = this.observableTimedSequence.subscribe(x => this.seconds++);
+    }
+
+    onStop() {
+        if(this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
 }
